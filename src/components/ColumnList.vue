187174be73a462177b046894f23931a22d61dd78
wzh0319 <2783341148@qq.com>
2021-06-01 17:07:48 +0800

    ColumnList组件封装

---
+++ src/components/ColumnList.vue
@@ -0,0 +1,36 @@
+<template>
+  <ul class="row">
+    <li v-for="column in list" :key='column.id' class="col">
+      <img :src="column.avatar" :alt="column.title">
+      <h5>{{column.title}}</h5>
+      <p>{{column.description}}</p>
+      <a href="#">进入专栏</a>
+    </li>
+  </ul>
+</template>
+<script lang='ts'>
+import { ref, defineComponent, PropType } from 'vue'
+import 'bootstrap/dist/css/bootstrap.min.css'
+export interface ColumnProps{
+  id: number;
+  title: string;
+  avatar: string;
+  description: string;
+}
+export default defineComponent({
+  name: 'app',
+  props: {
+    list: {
+      type: Array as PropType<ColumnProps[]>,
+      require: true
+    }
+  },
+  setup () {
+    const a = ref(1)
+    console.log(a)
+  }
+})
+</script>
+<style lang="scss" scoped>
+
+</style>
