2d03c431b7cb62f2e91b86e730e0d152ed44dea9
wzh0319 <2783341148@qq.com>
2021-06-02 16:37:14 +0800

    ColUmnList最后

--- src/App.vue
+++ src/App.vue
@@ -34,7 +34,7 @@ export default defineComponent({
         id: 3,
         title: 'test1的专栏',
         description: '这是的test1专栏，有一段非常有意思的简介，可以更新一下欧',
-        avatar: 'http://vue-maker.oss-cn-hangzhou.aliyuncs.com/vue-marker/5ee22dd58b3c4520912b9470.jpg?x-oss-process=image/resize,m_pad,h_100,w_100'
+        avatar: ''
       },
       {
         id: 4,
--- src/components/ColumnList.vue
+++ src/components/ColumnList.vue
@@ -1,20 +1,24 @@
 <template>
   <ul class="row">
-    <li v-for="column in list" :key='column.id' class="col">
-      <img :src="column.avatar" :alt="column.title">
-      <h5>{{column.title}}</h5>
-      <p>{{column.description}}</p>
-      <a href="#">进入专栏</a>
+    <li v-for="column in ColumnList" :key="column.id" class="col my-3">
+      <div class="card h-100  shadow-sm "  style="width: 18rem">
+        <div class="card-body">
+          <img :src="column.avatar" class="rounded-circle border  border-light w-25 my-3" />
+          <h5 class="card-title">{{ column.title }}</h5>
+          <p class="card-text">{{column.description}}</p>
+          <a href="#" class="btn btn-outline-primary">进入专栏</a>
+        </div>
+      </div>
     </li>
   </ul>
 </template>
-<script lang='ts'>
-import { ref, defineComponent, PropType } from 'vue'
+<script lang="ts">
+import { defineComponent, PropType, computed } from 'vue'
 import 'bootstrap/dist/css/bootstrap.min.css'
-export interface ColumnProps{
+export interface ColumnProps {
   id: number;
   title: string;
-  avatar: string;
+  avatar?: string;
   description: string;
 }
 export default defineComponent({
@@ -25,12 +29,19 @@ export default defineComponent({
       require: true
     }
   },
-  setup () {
-    const a = ref(1)
-    console.log(a)
+  setup (props) {
+    // 如果图片没有地址即需要操作props数据
+    const ColumnList = computed(() => {
+      return props.list?.map(column => {
+        if (!column.avatar) {
+          column.avatar = require('../assets/logo.png')
+          // console.log(column.avatar)
+        }
+        return column
+      })
+    })
+    return { ColumnList }
   }
 })
 </script>
-<style lang="scss" scoped>
-
-</style>
+<style lang="scss" scoped></style>
